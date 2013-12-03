65cca34e74aa58e4f03d2e81b0f30fd9fb024e8b
Tim <secoif@gmail.com>
2013-12-03 13:47:25 +1000

    Upgrade to streams2.

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 "use strict"
 
-var through = require('through')
-var stream = require('stream')
-var joiner = require('joiner-stream');
-var duplexer = require('duplexer')
+var through = require('through2')
+var PassThrough = require('readable-stream').PassThrough
+var duplexer = require('duplexer2')
 
-module.exports = function(fn) {
+module.exports = function(options, fn) {
+  if (!fn) fn = options, options = undefined
+  options = options || { objectMode: true, bubbleErrors: false }
+
   function between(streams) {
-    var aggregator = joiner();
-    // pipe all streams into joiner
-    var streamsArr = streams
-    // handle objects
-    if (!Array.isArray(streams)) {
-      streamsArr = Object.keys(streams).map(function(key) {return streams[key]})
-    }
-    streamsArr.forEach(function(aStream) {
-      aStream.pipe(aggregator)
+    var aggregator = new PassThrough(options)
+    // pipe all streams into aggregator
+    Object.keys(streams).forEach(function(key) {
+      if (streams[key].pipe) streams[key].pipe(aggregator)
     })
-    return duplexer(through(function(data) {
-      var result = fn.apply(null, arguments)
-      if (Array.isArray(streams)) result = parseInt(Number(result), 10)
+
+    var readable = through(options, function(data, enc, done) {
+      if (fn.length < 3) {
+        // sync
+        handleResult(null, fn.call(this, data, enc))
+      } else {
+        // async
+        fn.call(this, data, enc, handleResult)
+      }
+
+      function handleResult(err, result) {
+        if (err) return done(err)
+        if (Array.isArray(streams)) result = parseInt(Number(result), 10)
         var targetStream = streams[result]
-      if (targetStream) targetStream.write(data)
-    }), aggregator)
+        if (!targetStream) return done(new Error('no stream for ' + typeof result + ': ' + result))
+        targetStream.write(data)
+        done()
+      }
+    })
+
+    readable.on('error', function(err) {
+      // this prevents throw.
+      // errors seem to get bubbled to the duplexer anyway?
+      // note options.bubbleErrors is false?
+    })
+
+    return duplexer(options, readable, aggregator)
   }
+
   return between.between = between
 }
-
-
--- test/index.js
+++ test/index.js
@@ -6,19 +6,27 @@ var test = require('tape')
 var fs = require('fs')
 
 var from = require('from')
-var through = require('through')
+var through = require('through2').bind(null, {objectMode: true})
 var split = require('split')
+
+// wrapper to make through2 callback more like through
+function tf(fn) {
+  return function(data, enc, done) {
+    fn.call(this, data)
+    done()
+  }
+}
 
 test('it can switch between multiple streams', function(t) {
   t.plan(10)
   from([0,1,2,3,4,5,6,7,8,9])
   .pipe(Switchstream(function(data) {
     return !(data < 5)
-  }).between([through(function(x) {
+  }).between([through(tf(function(x) {
     t.ok(x < 5, x + ' < 5')
-  }), through(function(y) {
+  })), through(tf(function(y) {
     t.ok(y >= 5, y + ' >= 5')
-  })]))
+  }))]))
 })
 
 test('supports N streams', function(t) {
@@ -26,13 +34,13 @@ test('supports N streams', function(t) {
   from([0,1,2])
   .pipe(Switchstream(function(data) {
     return data
-  }).between([through(function(data) {
+  }).between([through(tf(function(data) {
     t.equal(data, 0)
-  }), through(function(data) {
+  })), through(tf(function(data) {
     t.equal(data, 1)
-  }), through(function(data) {
+  })), through(tf(function(data) {
     t.equal(data, 2)
-  })]))
+  }))]))
 })
 
 test('supports named streams', function(t) {
@@ -43,29 +51,32 @@ test('supports named streams', function(t) {
     if (data === 1) return 'one'
     if (data === 2) return 'two'
   }).between({
-    zero: through(function(data) {
+    zero: through(tf(function(data) {
       t.equal(data, 0)
-    }),
-    one: through(function(data) {
+    })),
+    one: through(tf(function(data) {
       t.equal(data, 1)
-    }),
-    two: through(function(data) {
-    t.equal(data, 2)
-    }),
+    })),
+    two: through(tf(function(data) {
+      t.equal(data, 2)
+    })),
   }))
 })
 
 test('does not crash if stream does not exist', function(t) {
-  t.plan(1)
+  t.plan(2)
   from([0,1])
   .pipe(Switchstream(function(data) {
     if (data === 0) return 'ok'
     if (data === 1) return 'not exist'
   }).between({
-    ok: through(function(data) {
+    ok: through(tf(function(data) {
       t.equal(data, 0)
-    })
+    }))
   }))
+  .on('error', function(err) {
+    t.ok(err)
+  })
 })
 
 test('joins streams back together', function(t) {
@@ -75,37 +86,40 @@ test('joins streams back together', function(t) {
   .pipe(Switchstream(function(data) {
     return data
   }).between({
-    a: through(function(data) {
+    a: through(tf(function(data) {
       t.equal(data, 'a')
       this.push(data.toUpperCase())
-    }),
-    b: through(function(data) {
+    })),
+    b: through(tf(function(data) {
       t.equal(data, 'b')
       this.push(data.toUpperCase())
-    })
+    }))
   }))
-  .pipe(through(function(data) {
+  .pipe(through(tf(function(data) {
     t.equal(input.shift().toUpperCase(), data)
-  }))
+  })))
 })
 
-
-//test('supports async', function(t) {
-  //t.plan(3)
-  //var inputStream = fs.createReadStream(__dirname + '/input.txt', 'utf8')
-  //inputStream._readableState.highWaterMark = 1
-  //inputStream
-  //.pipe(split(','))
-  //.pipe(switchstream(function(data, fn) {
-    //// wait a while
-    //setTimeout(function() {
-      //fn(null, data)
-    //}, data * 100)
-  //}, through(function(data) {
-    //t.looseEqual(data, 0)
-  //}), through(function(data) {
-    //t.looseEqual(data, 1)
-  //}), through(function(data) {
-    //t.looseEqual(data, 2)
-  //})))
-//})
+test('supports async', function(t) {
+  t.plan(3)
+  var inputStream = fs.createReadStream(__dirname + '/input.txt', 'utf8')
+  inputStream._readableState.highWaterMark = 1
+  inputStream
+  .pipe(split(','))
+  .pipe(Switchstream(function(data, enc, fn) {
+    // wait a while
+    setTimeout(function() {
+      fn(null, 'stream' + parseInt(data, 10))
+    }, data * 100)
+  }).between({
+    'stream0': through(tf(function(data) {
+      t.looseEqual(data, 0)
+    })),
+    'stream1': through(tf(function(data) {
+      t.looseEqual(data, 1)
+    })),
+    'stream2': through(tf(function(data) {
+      t.looseEqual(data, 2)
+    }))
+  }))
+})
